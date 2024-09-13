feb191c6b43ca3750dc13d050801ac24da0a9a3e
linpeic <joycelin02012005@gmail.com>
2024-09-13 11:33:30 +0800

    oakMe.js

--- HW/HW1/oakMe.js
+++ HW/HW1/oakMe.js
@@ -6,9 +6,7 @@ app.use((ctx) => {
   // ctx.response.status = 404
   console.log('url=', ctx.request.url)
   let pathname = ctx.request.url.pathname
-  if(pathname == '127.0.0.1:8000'){
-    ctx.response.body = '找不到找不到找不到找不到找不到找不到'
-  }
+ 
   if (pathname == '/name') {
     ctx.response.body = '姓名：林沛欣'
   } 
@@ -18,7 +16,9 @@ app.use((ctx) => {
   if(pathname == '/gender'){
     ctx.response.body = '性別：女'
   }
-  
+  else{
+    ctx.response.body = '找不到找不到找不到找不到找不到找不到'
+  }
   // ctx.response.body = 'Not Found!'
 });
 
