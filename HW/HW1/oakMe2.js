48422a031909dae97dd5fcfadb15116446e445ed
linpeic <joycelin02012005@gmail.com>
2024-09-13 20:05:29 +0800

    oakMe.js

--- HW/HW1/oakMe2.js
+++ HW/HW1/oakMe2.js
@@ -15,18 +15,26 @@ app.use((ctx) => {
 <li><a href='/age'>年齡<a></li>
 <li><a href='/gender'>性別<a></li>
 <li><a href='/music'>音樂<a></li>
+<li><a href='/dream'>夢想<a></li>
 </ol>
 </body>
 </html>
 `
-  } else if (pathname == '/name') {
+  } 
+  else if (pathname == '/name') {
     ctx.response.body = '林沛欣'
-  } else if (pathname == '/age') {
+  } 
+  else if (pathname == '/age') {
     ctx.response.body = '19'
-  } else if (pathname == '/gender') {
+  } 
+  else if (pathname == '/gender') {
     ctx.response.body = '女'
-  } else if (pathname == '/music') {
-    ctx.response.body = ''
+  } 
+  else if (pathname == '/music') {
+    ctx.response.redirect('https://youtu.be/rlRSJNHS40s?si=1DGpVnq2P5qgG7Ke')
+  } 
+  else if (pathname == '/dream') {
+    ctx.response.body = '養狗'
   } 
   // ctx.response.body = 'Not Found!'
 });
